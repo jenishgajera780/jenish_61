255c7eecda20c925429dd6e4b3811a36e4cf3574
jenishgajera780 <136606761+jenishgajera780@users.noreply.github.com>
2023-06-29 18:55:46 +0530

    Update J61_5.C

--- J61_5.C
+++ J61_5.C
@@ -1,3 +1,22 @@
-//
+//Q5  write a program to print leap years between two give numbers using while loop.
+#include<stdio.h>
+#include<conio.h>
 
-#
+void main(){
+int x,y,z;
+
+clrscr();
+
+printf("enter first year");
+scanf("%d",&x);
+printf("enter second year: ");
+scanf("%d",y);
+printf("leap year %d and %d are \n",x,y);
+ for(z=x; z<=x; z++){
+   if(((a%4==0)&&(a%100!=0))||(a%400 == 0));
+   }{
+   printf("%d\n",z);
+   }
+   
+   getch();
+   }
