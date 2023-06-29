ee5a105186e00b5f23255b0257ecbc46ab1a53bd
jenishgajera780 <136606761+jenishgajera780@users.noreply.github.com>
2023-06-29 18:34:22 +0530

    Add files via upload

---
+++ 61_2JENI.C
@@ -0,0 +1,18 @@
+// Q2 write a program to print n to using a while loop.
+
+#include<stdio.h>
+#include<conio.h>
+
+void main(){
+int j,e;
+
+ clrscr();
+ e = 1;
+ printf("enter any number");
+ scanf("%d",&j);
+ while(j>=e){
+ printf("%d\n",j);
+ j--;}
+
+getch();
+}
