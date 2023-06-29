8b6dadb89b6b2c9919e8bfaf563bf39e31e61006
jenishgajera780 <136606761+jenishgajera780@users.noreply.github.com>
2023-06-29 18:47:12 +0530

    Update 61_3JENI.C

--- 61_3JENI.C
+++ 61_3JENI.C
@@ -6,18 +6,12 @@
 void main(){
 int j;
 int e = 1;
+
 clrscr();
-
-printf("enter any number:");
- scanf("%d",&j);
-do
-{  
-if(e%2==0){ 
-printf("%d\n",e);
-
+ printf("enter any number");
+scanf ("%d",j);
+do{ printf("%d\n",e);
 e++;
-
-}while(j>=e);
-
+}while(e<=j);
 getch();
 }
